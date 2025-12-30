0380fc54f088014e22e657da9c347d3bbaa7928b
Carlos Orrego <carlosorrego5151@gmail.com>
2025-12-29 23:55:22 -0500

    feat: add base flujo bancario

---
+++ 0.ejercicios/1.flujo-bancario/1.flujo-bancario.dart
@@ -0,0 +1,33 @@
+
+// Cuenta debe tener 
+// - titular
+// - cedula
+// - tipo de cuenta
+// - No Cuenta
+// - Saldo
+// - Tipo de moneda
+
+// Operaciones 
+// - Depositar dinero a la cuenta en el tipo de moneda
+// - Retirar dinero de la cuenta
+// - Consultar el saldo actual 
+
+// Transferencias 
+// - Dinero a otra cuenta
+// - La transferencia debe obedecer a la logica (fecha, id transaccion, que el saldo de controle en ambas cuentas)
+// - Permitir consultar las transferencias
+
+// Historial de transacciones
+// - Historial detallado que liste todas las operaciones y transferencias
+// - Debe tener (tipo, monto, fecha, moneda, cuenta de origen, cuenta destino, descripcion, id transaccion)
+// Iniciar sesion
+// - Cada usuario al crear la cuenta puede crear un username unico
+// - La contraseña se asigna por defecto
+// - La contraseña puede ser cambiada
+
+// Validaciones
+// - Montos negativos no permitidos
+// - No permitir transacciones sin cuenta destino
+// - Apagar opciones si no se pueden realizar por falta de fondos
+ 
+
