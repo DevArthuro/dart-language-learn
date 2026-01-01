d69afa0fd1d1f68b1314c7d9a913d37badcf7df1
Carlos Orrego <carlosorrego5151@gmail.com>
2025-12-31 20:39:28 -0500

    feat: initialize http project first practice

---
+++ 12.http/paquete/bin/paquete.dart
@@ -0,0 +1,5 @@
+import 'package:paquete/paquete.dart' as paquete;
+
+void main(List<String> arguments) {
+  print('Hello world: ${paquete.calculate(5, 6)}!');
+}
---
+++ 12.http/paquete/test/paquete_test.dart
@@ -0,0 +1,8 @@
+import 'package:paquete/paquete.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
